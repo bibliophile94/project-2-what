c0ac387c69ab2e3a65fb608999e15a66c2b8e0fc
bibliophile94 <kstolpe@mail.bradley.edu>
2015-10-15 09:58:05 -0500

    rudimentary fortune teller

--- what.js
+++ what.js
@@ -1,4 +1,5 @@
 var textarea = document.getElementById('what');
+var replyarea = document.getElementById('result');
 var form = document.getElementsByTagName('form')[0];
 textarea.addEventListener('change', function() {
     form.submit();
@@ -8,3 +9,24 @@ what = what.replace('?what=', '');
 what = what.replace(/\+/g, ' ');
 what = decodeURIComponent(what);
 textarea.value = what;
+what = what.toLowerCase();
+what = what.replace(/[.,-\/#!$%\^&\*;:{}=\-_`~'" ?|()]/g,"")
+
+if (what == "willimeettheone")
+replyarea.value = "You will die emotionally alone";
+else if (what == "willipassdansclass")
+replyarea.value = "With a project like this? Unlikely.";
+else if (what == "willigetrich")
+replyarea.value = "Your money managment skills leave much to be desired.";
+else if (what == "willmyfathereverloveme")
+replyarea.value = "You were a last ditch effort to save a failing marriage. He will continue to resent you for the lasting tie you created between him and your mother.";
+else if (what == "willilivealonglife")
+replyarea.value = "With your eating habits and lifestyle?";
+else if (what == "")
+	replyarea.value = "";
+else if (what == "willtrumpwintheelection")
+	replyarea.value = "With our luck? Probably.";
+
+else
+	replyarea.value = "Probably not";
+	
